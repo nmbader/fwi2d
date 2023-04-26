08913f8c03e32206c46fede0956cc5cce1a5dcd5
Milad Bader <nmbader@stanford.edu>
2023-04-26 15:52:04 -0700

    Add remove.py as an alternative to SEP Rm program

---
+++ src/python/remove.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.6
+import os
+import sys
+import argparse
+import seppy
+
+def parse_args():
+  args=[]
+  files=[]
+  for arg in sys.argv[1:]:
+    if arg =="-f":  args.append("force")
+    elif arg =="-i":  args.append("interactive")
+    elif arg =="-q":  args.append("quiet")
+    elif arg =="-v":  args.append("verb")
+    else: files.append(arg)
+  if files == None: print("Nothing to be deleted")
+  return args,files
+
+def remove_file(file):
+  sep = seppy.sep()
+  faxes = sep.read_header(file)
+  os.remove(sep.hdict["in"])
+  os.remove(file)
+
+if __name__ == "__main__":
+  
+  args,files=parse_args()
+  for file in files:
+    remove_file(file)
+  sys.exit(0)
